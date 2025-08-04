652727ccf39008320366be99a6da7297e3b5859e
AliAyali <ali.ayali.programmer@gmail.com>
2025-08-04 06:01:50 -0700

    feat(ui): Splash Screen UI Design

--- app/src/main/java/com/aliayali/market_baz/MainActivity.kt
+++ app/src/main/java/com/aliayali/market_baz/MainActivity.kt
@@ -3,24 +3,26 @@ package com.aliayali.market_baz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.aliayali.market_baz.navigation.SetupNavigation
 import com.aliayali.market_baz.ui.theme.Market_bazTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             Market_bazTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    SetupNavigation(
+                        innerPadding,
+                        navController
+                    )
                 }
             }
         }
